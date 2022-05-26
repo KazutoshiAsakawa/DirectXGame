1d1600155b8ea0c1a1fab32b098d6a5968cf2fa4
KazutoshiAsakawa <k020g1206@g.neec.ac.jp>
2022-05-26 14:20:50 +0900

    3-3

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -4,10 +4,23 @@
 class PostEffect :
 	public Sprite
 {
+	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 public:
 	PostEffect();
 
+	/// <summary>
+	/// 初期化
+	/// <summary>
+	void Initialize();
+
 	//void Draw(DirectXCommon* dxCom);
 	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private: // メンバ変数
+	// テクスチャバッファ
+	ComPtr<ID3D12Resource> texBuff;
+	// SRV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
